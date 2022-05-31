39392957eaf49a42bf2483af1fe3cb66946fbdc1
ABHISHEK ROY <98046344+royabhishek47@users.noreply.github.com>
2022-05-31 19:03:03 +0530

    Add files via upload

---
+++ Check if a string contains all binary codes of size k.java
@@ -0,0 +1,12 @@
+//Check if a string contains all binary codes of size k
+
+class Solution {
+	public boolean hasAllCodes(String s, int k) {
+		int combinations = (int)Math.pow(2,k);
+		HashSet<String> set = new HashSet<>();
+		for(int i=0;i<s.length()-k+1;i++){
+			set.add(s.substring(i,i+k));
+		}
+		return set.size()==combinations;
+	}
+}
