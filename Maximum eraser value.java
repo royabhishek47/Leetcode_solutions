8db992b7e73fc5675dcb832d93689e888e8bd400
ABHISHEK ROY <98046344+royabhishek47@users.noreply.github.com>
2022-06-12 11:05:32 +0530

    Add files via upload

---
+++ Maximum eraser value.java
@@ -0,0 +1,20 @@
+class Solution {
+public int maximumUniqueSubarray(int[] nums) {
+int ans = 0;
+int left = 0;
+HashSet set = new HashSet();
+int sum = 0;
+for(int right = 0; right < nums.length; right++){
+while(set.contains(nums[right])){
+set.remove(nums[left]);
+sum -= nums[left++];
+}
+sum += nums[right];
+set.add(nums[right]);
+
+        ans = Integer.max(ans, sum);
+    }
+    
+    return ans;
+}
+}
