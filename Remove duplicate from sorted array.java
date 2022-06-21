24c4cb82cbf920aa331bd04868104799d837a8bf
Abhishek Roy <98046344+royabhishek47@users.noreply.github.com>
2022-06-21 19:31:03 +0530

    Add files via upload

---
+++ Remove duplicate from sorted array.java
@@ -0,0 +1,18 @@
+//Remove duplicate from sorted array
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int j=0;
+        for(int i=0; i< nums.length ; i++){
+            nums[j] = nums[i];
+            
+            while(i < nums.length && nums[j] == nums[i] ){
+                i++;
+            }
+            i--;
+            j++;
+        }
+        return j;
+        
+    }
+}
