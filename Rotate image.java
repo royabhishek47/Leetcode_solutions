c9e66c1e3b28f18c98e11c637624562ce58d57d7
Abhishek Roy <98046344+royabhishek47@users.noreply.github.com>
2022-06-22 19:15:49 +0530

    Add files via upload

---
+++ Rotate image.java
@@ -0,0 +1,22 @@
+//Rotate image
+
+class Solution {
+public void rotate(int[][] arr) {
+int len = arr.length,temp;
+for(int i=0;i<len;i++){
+for(int j=i;j<len;j++){
+temp = arr[i][j];
+arr[i][j] = arr[j][i];
+arr[j][i] = temp;
+}
+}
+
+    for(int i=0;i<len;i++){
+        for(int j=0;j<len/2;j++){
+            temp = arr[i][j];
+            arr[i][j] = arr[i][len-j-1];
+            arr[i][len-j-1] = temp;
+        }
+    }        
+}
+}
