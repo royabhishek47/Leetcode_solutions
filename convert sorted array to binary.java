91edb1f63e56f386071cb6fa77dc1e039060b141
Abhishek Roy <98046344+royabhishek47@users.noreply.github.com>
2022-08-11 21:45:39 +0530

    Add files via upload

---
+++ convert sorted array to binary.java
@@ -0,0 +1,23 @@
+class Solution {
+public TreeNode sortedArrayToBST(int[] nums) {
+
+    if(nums.length == 0){
+        return null;
+    }
+    return bst(nums,0,nums.length-1);
+    
+}
+
+public TreeNode bst(int nums[] , int left , int right){
+    
+    if(left > right){
+        return null;
+    }
+    
+    int mid = left + (right - left)/2;
+    TreeNode root = new TreeNode(nums[mid]);
+    root.left = bst(nums,left,mid-1);
+    root.right = bst(nums,mid+1,right);
+    return root;
+}
+}
